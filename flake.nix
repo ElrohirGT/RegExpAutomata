0a0a57f4f88741e0f36ff711c50660ee46722626
ElrohirGT <elrohirgt@gmail.com>
2024-08-16 12:04:20 -0600

    feat: Added flake file

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "RegExp Automata Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    forAllSystems = {
+      pkgs ? nixpkgs,
+      function,
+    }:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "x86_64-macos"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ]
+      (system:
+        function {
+          pkgs = import pkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [
+              #inputs.something.overlays.default
+            ];
+          };
+          inherit system;
+        });
+  in {
+    devShells = forAllSystems {
+      function = {pkgs, ...}: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [python3];
+        };
+      };
+    };
+  };
+}
