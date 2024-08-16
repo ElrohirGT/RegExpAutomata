449afec222545517974ab9b5235de38e577f7293
ElrohirGT <elrohirgt@gmail.com>
2024-08-16 13:45:25 -0600

    feat: Added tests

--- modules/tests.py
+++ modules/tests.py
@@ -1,20 +1,15 @@
 import unittest
+from shunYard import toPostFix
 
-class TestStringMethods(unittest.TestCase):
+# All tests will be added to this file!
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+class TestShuntingYard(unittest.TestCase):
+    def test_basic_example(self):
+        infix = "a+b"
+        expected = "ab+"
+        actual = toPostFix(infix)
+        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
+    # print(toPostFix)
     unittest.main()
